9c4e48c3cbacdf881ef4a98d25b9f82fdf6e0745
Gonzalez Salvador <salvadorsopas@gmail.com>
2025-02-16 23:16:26 -0600

    Añadiendo endpoint peticion HTTP POST '/api/homes' que permitira crear objetos para publicaciones de nuevas casas realizado con validacion de datos [PROOF] POSTMAN CLIENT persistencia de datos en archivo JSON

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors')
 const fs = require('fs');   // Modulo para leer archivos
 const data = require('./data.json');
+const { error } = require('console');
+const { title } = require('process');
 
 // Inicializamos la appa de express
 const app = express();
@@ -16,6 +18,46 @@ app.use(express.json())
 app.get('/api/homes', (req, res) =>{
     res.json(data)
 })
+
+/* POST /api/homes
+    * Crea una nueva casa en el archivo data.json
+*/
+app.post('/api/homes', (req,res)=>{
+    // Obtenemos los datos de la casa a crear
+    const {title, price, location, bedrooms, bathrooms, square_feet, image} = req.body;
+
+    // Validamos que todos los campos sean obligatorios
+    if(!title || !price || !location || !bedrooms || !bathrooms || !square_feet || !image){
+        return res.status(400).json({error: "Todos los campos son obligatorio"})
+    }
+    // Obtenemos el mayor ID de las casas y sumamos 1 para asiganar un 1 unico y creciente 
+    //const newId = data.length > 0 ? Math.max(...data.map(home => home.id)) + 1 : 1;
+
+    // Creamos el (nuevo objeto) ...la nueva casa con los datos enviados por el cliente
+    const newHome = {
+        id: data.length > 0 ? data[data.length -1].id +1 : 1,
+        title,  
+        price,
+        location,
+        bedrooms,
+        bathrooms,
+        square_feet,
+        image
+    }
+    // agregamos la nueva priopiedad al array de datos
+    data.push(newHome);
+
+    // Guardamos los datos actualizados en el archivo 'data.json'
+    fs.writeFile('./data.json', JSON.stringify(data, null, 2), (error) =>{
+        if(error){
+            console.error('Error al guardar los datos', error);
+           return res.status(500).json({error : 'Error al guardar los datos'})
+        }
+        // Respondemos al cliente con la nueva casa creada
+        res.status(201).json(newHome)
+        })
+    })
+
 
 app.listen(PORT, () =>{
     console.log(` API de SmatHouse corriendo en http://localhost:${PORT} `)
