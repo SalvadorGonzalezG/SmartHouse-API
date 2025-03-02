94c3a8dccb79e517ea1b857c0ab322ac45ffcf37
Gonzalez Salvador <salvadorsopas@gmail.com>
2025-03-01 23:57:21 -0600

    [ADD] funcion inicio de sesion

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,17 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jwt");
-const userMode = require("../models/userModel")
+const userModel = require("../models/userModel")
 
 // Logica para realizar el login
+
+const SECRET_KEY = "*.sAlVaDor.05"
+
+// f inicio de sesion
+const login = (req, res) =>{
+    const {email, password} = req.body;
+
+    //validacion de los datos 
+    if(!email || !password){
+        return res.json(400).json({error: "Correo & contraseña son obligatorio"})
+    }
+}
